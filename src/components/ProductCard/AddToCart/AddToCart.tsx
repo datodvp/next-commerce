a59cb1fa4a6da0ba6bd11165f2d898ece308ab7a
Davit Gagua <daviti.dev@gmail.com>
2025-04-06 19:22:30 +0400

    add loading animation for add to cart button

--- src/components/ProductCard/AddToCart/AddToCart.tsx
+++ src/components/ProductCard/AddToCart/AddToCart.tsx
@@ -1,20 +1,32 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from "./styles.module.scss";
-import { faBasketShopping } from "@fortawesome/free-solid-svg-icons";
+import { faBasketShopping, faSpinner } from "@fortawesome/free-solid-svg-icons";
 import { IProduct } from "@/models/common/types";
+import { useState } from "react";
 
 interface IProps {
   product: IProduct;
 }
 
 const AddToCart = ({ product }: IProps) => {
-  const addToCart = (product: IProduct) => {
-    // console.log(product);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const addToCart = () => {
+    setIsLoading(true);
+    setTimeout(() => {
+      console.log(product);
+      setIsLoading(false);
+    }, 500);
   };
+
   return (
     <div className={styles.root}>
-      <button onClick={() => addToCart(product)} className={styles.addToCart}>
-        <FontAwesomeIcon icon={faBasketShopping} width={16} height={16} />
+      <button onClick={addToCart} className={styles.addToCart}>
+        {isLoading ? (
+          <FontAwesomeIcon icon={faSpinner} width={16} height={16} spin />
+        ) : (
+          <FontAwesomeIcon icon={faBasketShopping} width={16} height={16} />
+        )}
       </button>
     </div>
   );
